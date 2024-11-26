c005ffb84362963aabb1f4df674fd2a7106ad55f
OmarKHDR <omarsliman37@gmail.com>
2024-11-26 12:29:55 +0200

    fwe

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,14 +3,15 @@
     fields: a list of strings representing all fields to obfuscate
     redaction: a string representing by what the field will be obfuscated
     message: a string representing the log line
-    separator: a string representing by which character is separating all fields in the log line (message)
+    separator: a string representing by which
 '''
 import re
+from typing import List, Any
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List, redaction: str,
+                 message: str, separator: str) -> str:
     '''Heloo wthern f mdf d'''
-    pattern = [f'(?<={st}=).*?(?={separator})' for st in fields]
-    pattern = '|'.join(pattern)
+    pattern: str = '|'.join([f'(?<={st}=).*?(?={separator})' for st in fields])
     print(pattern)
-    return re.sub(pattern,redaction, message,)
+    return re.sub(pattern, redaction, message)
