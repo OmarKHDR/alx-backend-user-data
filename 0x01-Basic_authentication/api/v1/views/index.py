5b532e48da819c12e742ce1dcb4090aafea1ff13
OmarKHDR <omarsliman37@gmail.com>
2024-11-27 13:36:44 +0200

    hello

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,10 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/api/v1/unauthorized/', strict_slashes=False)
+def unauth():
+    """ Gdg gds asd
+    """
+    abort(401)
