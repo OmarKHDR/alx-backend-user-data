695d3e52508369edd40a6db0984fb5571e33d955
OmarKHDR <omarsliman37@gmail.com>
2024-11-28 09:35:26 +0200

    hello

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,8 +7,12 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ This wasnt documented
     """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
-        if authorization_header is None or not isinstance(authorization_header, str):
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """This for checkigner euth auth
+        """
+        if authorization_header is None or\
+                not isinstance(authorization_header, str):
             return None
         auth_word = authorization_header.split()
         if len(auth_word) != 2 or auth_word['0'] != 'Basic':
