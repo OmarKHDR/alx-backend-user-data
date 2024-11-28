8cb257caba900dd1f3f04cdd8fdb635f984427b5
OmarKHDR <omarsliman37@gmail.com>
2024-11-28 10:00:12 +0200

    hello

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """This is basic of basics
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -18,3 +19,16 @@ class BasicAuth(Auth):
         if len(auth_word) != 2 or auth_word[0] != 'Basic':
             return None
         return authorization_header.strip("Basic ")
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ THis is for some decode
+        """
+        if base64_authorization_header is None or\
+                not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            code = base64.b64decode(base64_authorization_header)
+            return code.decode()
+        except Exception:
+            return None
